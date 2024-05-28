92dafc1d678b1d30657e9b93ab13c971229794c3
Pablinox <pablojarab95@gmail.com>
2024-05-27 20:12:49 -0500

    se adiciono el boton para mostrar el grafico precio vs modelo

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv(
-    'C:/Users/pablo/Documents/my-data-analysis-project/vehicles_us.csv')
+car_data = pd.read_csv("vehicles_us.csv")
 
-st.header('Cuadro de mandos de análisis de vehículos')
+st.header('Graficos con infromacion importante de vehiculos')
 
 hist_button = st.button('Construir histograma')
 if hist_button:
@@ -17,4 +16,10 @@ scatter_button = st.button('Construir gráfico de dispersión')
 if scatter_button:
     st.write('Gráfico de dispersión de precio vs kilómetros recorridos')
     fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+if scatter_button:
+    st.write('Gráfico de dispersión de precio vs Modelo')
+    fig = px.scatter(car_data, x='model_year', y='price')
     st.plotly_chart(fig)
